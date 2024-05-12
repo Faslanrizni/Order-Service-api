f6509ed2042cda23dee9b8acf8dcb960e0049d31
Faslanrizni <116054643+Faslanrizni@users.noreply.github.com>
2024-05-12 13:05:46 +0530

    successfully connected to eureka server

--- src/main/java/com/devstack/orderserviceapi/OrderServiceApiApplication.java
+++ src/main/java/com/devstack/orderserviceapi/OrderServiceApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class OrderServiceApiApplication {
 
 	public static void main(String[] args) {
